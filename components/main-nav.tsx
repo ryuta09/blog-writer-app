6c38d8b6bf0fbd02b768c3b8275c4fe62f148ab1
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-10-23 21:39:50 +0900

    feat: メニューの作成

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,10 +1,15 @@
+"use client"
 import { NavItem } from "@/types";
 import Link from "next/link";
+import MobileNav from "./mobile-nav";
+import { useState } from "react";
 interface MainNavProps {
   items: NavItem[]
   children?: React.ReactNode
 }
 export default function MainNav({items}: MainNavProps) {
+
+  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false)
   return(
     <div className="flex items-center md:gap-10">
       <Link href={"/"} className="hidden md:flex items-center"><span className="font-bold hidden sm:inline-block">PostWriter</span></Link>
@@ -13,6 +18,12 @@ export default function MainNav({items}: MainNavProps) {
           <Link key={index} href={item.href} className="text-lg sm:text-sm font-medium hover:text-foreground/80">{item.title}</Link>
         ))}
       </nav>
+      <button className="md:hidden" onClick={() => setShowMobileMenu(prev => !prev)}>
+        <span>メニュー</span>
+      </button>
+        {showMobileMenu && (
+          <MobileNav items={items}/>
+        )}
     </div>
   )
 }
